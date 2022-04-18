2102af2f689d9bc1a6bff4a1166daa78ba86c273
Kiko <kikoguirado@gmail.com>
2022-04-18 21:20:28 +0200

    finished

--- src/app/componentes/task-component/task-component.component.ts
+++ src/app/componentes/task-component/task-component.component.ts
@@ -4,27 +4,24 @@ import { Task } from '../../modelos/task';
 
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { TaskService } from '../../servicios/task-service.service';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-task-component',
   templateUrl: './task-component.component.html',
-  styleUrls: ['./task-component.component.css']
+  styleUrls: ['./task-component.component.css'],
 })
 export class TaskComponentComponent implements OnInit {
-
   @Input() task!: Task;
 
   faTrashAlt = faTrashAlt;
 
-  constructor(public taskService: TaskService, private toastr: ToastrService) { }
+  constructor(public taskService: TaskService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   deleteTask(task: Task) {
-    this.taskService.deleteTask(task);
-    this.toastr.success('Task deleted successfully.', 'Success!');
+    if (confirm('¿Estás seguro de que quieres eliminar esta tarea?')) {
+      this.taskService.deleteTask(task);
+    }
   }
-
 }
--- src/app/servicios/task-service.service.ts
+++ src/app/servicios/task-service.service.ts
@@ -6,24 +6,43 @@ import { Task } from '../modelos/task';
   providedIn: 'root',
 })
 export class TaskService {
-  tasks: Task[] = [];
+  tasks: Task[];
 
   constructor() {
-    
+    this.tasks = [
+      {title: 'Create a Website', description: 'Create a wordpress website'},
+      {title: 'write a document', description: 'Do other stuff'}
+    ];
   }
 
   getTasks() {
+    if(localStorage.getItem('tasks') === null) {
+      this.tasks = [];
+    } else {
+      this.tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+    }
     return this.tasks;
   }
 
   addTask(task: Task) {
     this.tasks.push(task);
+    let tasks = [];
+    if(localStorage.getItem('tasks') === null) {
+      tasks = [];
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+      tasks.push(task); 
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
   }
 
   deleteTask(task: Task) {
-    for(let i = 0; i < this.tasks.length; i++) {
-      if(this.tasks[i] === task) {
+    for (let i = 0; i < this.tasks.length; i++) {
+      if (task == this.tasks[i]) {
         this.tasks.splice(i, 1);
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
       }
     }
   }
